3322e6104e57d4d7fb000524c680347d6b24edfb
Daniel Santos <daniel17sants@gmail.com>
2020-05-01 10:56:27 -0300

    Continuing todo App

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -13,7 +14,16 @@ export default function App() {
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
       return prevTodos.filter(todo => todo.key != key);
-    })
+    });
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos,
+      ];
+    });
   }
 
   return (
@@ -22,6 +32,9 @@ export default function App() {
       <Header />
       <View style={styles.content}>
         {/* to form */}
+        <AddTodo submitHandler={submitHandler} />
+
+        {/* to list */}
         <View style={styles.list}>
           <FlatList
             data={todos}
